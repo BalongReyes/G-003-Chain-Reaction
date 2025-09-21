e38849fc454c69b9a5ebeb80f0986bf9af2ad82c
Sedian Paul P. Reyes <50647987+BalongReyes@users.noreply.github.com>
2025-09-21 21:42:24 +0800

    Version 1.5

--- src/MainSystem/Object/CellType/CellDuplicator.java
+++ src/MainSystem/Object/CellType/CellDuplicator.java
@@ -2,20 +2,27 @@ package MainSystem.Object.CellType;
 
 import DataSystem.Data.Player;
 import DataSystem.Type.TypeCellPart;
+import MainSystem.Methods.MethodsNumber;
 import MainSystem.Object.Cell;
 import ManagerSystem.Handlers.HandlerObject.HandlerCell;
 import ManagerSystem.Manager.ManagerCell.ManagerDuplicator;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.util.List;
 
 public class CellDuplicator extends Cell {
 
     public ManagerDuplicator managerDuplicator;
     private boolean isSyncing = false;
+    private int animationTick = 0;
+    private int startAngle = 0;
 
     public CellDuplicator(double x, double y, int rx, int ry) {
         super(x, y, rx, ry);
         cellPart = TypeCellPart.duplicator;
         managerDuplicator = new ManagerDuplicator(this);
+        startAngle = MethodsNumber.getRandomNumber(0, 359);
     }
 
     public ManagerDuplicator getManagerDuplicator() {
@@ -71,5 +78,32 @@ public class CellDuplicator extends Cell {
                 duplicatorCell.setPop(popPlayer);
             }
         }
+    }
+
+    @Override
+    protected void tickAnimations() {
+        super.tickAnimations();
+        animationTick++;
+        if (animationTick > 50) {
+            animationTick = 0;
+            startAngle = (startAngle + 5) % 360;
+        }
+    }
+
+    @Override
+    public void renderLayer2(Graphics2D g) {
+        super.renderLayer2(g);
+        if (isCellPart(TypeCellPart.space)) {
+            return;
+        }
+        drawDesign(g);
+    }
+
+    private void drawDesign(Graphics2D g) {
+        g.setColor(Color.lightGray);
+        g.setStroke(new BasicStroke(2.0F));
+        g.drawArc(getX() + 5, getY() + 5, 30, 30, startAngle, 90);
+        g.drawArc(getX() + 5, getY() + 5, 30, 30, startAngle + 180, 90);
+        g.setStroke(new BasicStroke(1.0F));
     }
 }
